609f7e1e06d22e56e3d4304f31f78307b8dfdf04
yuanxiangweidehithub <34850188+yuanxiangweidehithub@users.noreply.github.com>
2019-07-02 17:20:27 +0800

    新增末班文件

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const Koa = require('koa')
 const app = new Koa()
+const { normal } = require('./tpl')
+const ejs = require('ejs')
 app.use(async (ctx,next)=>{
-	ctx.body='首页'
+	ctx.type = 'text/html;charset:utf-8'
+	ctx.body=ejs.render(normal,{
+		name:'454646544545'
+	})
 })
 app.listen(4455)
