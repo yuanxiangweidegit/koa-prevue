64c04fc96f94ac80c374d227111ee04c85f0d45b
yuanxiangweidehithub <34850188+yuanxiangweidehithub@users.noreply.github.com>
2019-07-04 09:43:03 +0800

    xiugai

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Koa = require('koa')
 const app = new Koa()
-const { normal } = require('./tpl')
-const ejs = require('ejs')
+const views =require('koa-views')
+const { resolve }=require('path')
+//const ejs = require('ejs')
+app.use(views(resolve(__dirname,'./views'),{
+	extension:'ejs'
+}))
 app.use(async (ctx,next)=>{
-	ctx.type = 'text/html;charset:utf-8'
-	ctx.body=ejs.render(normal,{
-		name:'454646544545'
+	await ctx.render('index',{
+		name:'zhangsan'
 	})
 })
 app.listen(4455)
